9386a66f8429ea1c22f614495271ab18b802251d
Victor Barros <victorwbarros@gmail.com>
2023-09-19 23:12:05 -0300

    feat: :sparkles: create main container
    create a container to handle the main content, is a sidebar sibling

---
+++ src/components/general/MainContainer.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="w-full">
+    <Topbar/>
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import Topbar from './Topbar.vue';
+
+export default {
+  components: {
+    Topbar
+  }
+}
+</script>
+<style lang="">
+  
+</style>
