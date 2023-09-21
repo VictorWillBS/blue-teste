f1b71c5d0bc93957c4f7cbf7f2350a9add716279
Victor Barros <victorwbarros@gmail.com>
2023-09-21 03:59:09 -0300

    feat : :sparkles: add exit button

--- src/components/general/Sidebar.vue
+++ src/components/general/Sidebar.vue
@@ -13,42 +13,54 @@
         :class="mobileSidebarOpen ? '' : 'hidden'"
         tabindex="0"
     >
-        <section class="h-full w-40 flex-none bg-white p-3 shadow lg:block">
-            <article class="mb-20 flex w-full items-center justify-center">
-                <img
-                    src="../../assets/blue-logo.svg"
-                    class="w-25 h-8"
+        <section
+            class="flex h-full w-40 flex-col justify-between bg-white p-3 shadow"
+        >
+            <div>
+                <article class="mb-20 flex w-full items-center justify-center">
+                    <img
+                        src="../../assets/blue-logo.svg"
+                        class="w-25 h-8"
+                    />
+                </article>
+                <section class="flex items-center justify-center">
+                    <ul class="">
+                        <a class="mb-4 flex w-full items-center justify-start">
+                            <ViewDashboard
+                                :size="30"
+                                class="me-3"
+                            />
+                            <span class="text-sm font-bold">Dashboard</span>
+                        </a>
+                        <a
+                            class="mb-4 flex w-full items-center justify-start text-gray-500"
+                        >
+                            <TableIcon
+                                :size="30"
+                                class="me-3"
+                            />
+                            <span class="text-sm font-bold">Vendas</span>
+                        </a>
+                        <a
+                            class="mb-4 flex w-full items-center justify-start text-gray-500"
+                        >
+                            <MoneyBagIcon
+                                :size="30"
+                                class="me-3"
+                            />
+                            <span class="text-sm font-bold">Planos</span>
+                        </a>
+                    </ul>
+                </section>
+            </div>
+            <button class="mb-10 flex items-center justify-start gap-x-4">
+                <ion-icon
+                    name="exit"
+                    size="large"
+                    class="icon-exit"
                 />
-            </article>
-            <section class="flex items-center justify-center">
-                <ul class="">
-                    <a class="mb-4 flex w-full items-center justify-start">
-                        <ViewDashboard
-                            :size="30"
-                            class="me-3"
-                        />
-                        <span class="text-sm font-bold">Dashboard</span>
-                    </a>
-                    <a
-                        class="mb-4 flex w-full items-center justify-start text-gray-500"
-                    >
-                        <TableIcon
-                            :size="30"
-                            class="me-3"
-                        />
-                        <span class="text-sm font-bold">Vendas</span>
-                    </a>
-                    <a
-                        class="mb-4 flex w-full items-center justify-start text-gray-500"
-                    >
-                        <MoneyBagIcon
-                            :size="30"
-                            class="me-3"
-                        />
-                        <span class="text-sm font-bold">Planos</span>
-                    </a>
-                </ul>
-            </section>
+                <p class="pt-0.5 text-sm font-bold">Sair</p>
+            </button>
         </section>
         <div
             class="w-full bg-black bg-opacity-30 lg:hidden"
@@ -84,7 +96,7 @@
     })
 </script>
 <style lang="css">
-    .icon {
-        fill: #6b7280;
+    .icon-exit {
+        fill: #ff1717;
     }
 </style>
