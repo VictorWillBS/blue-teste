248d666a6c935e0f72a28449c5322e96173514d5
Victor Barros <victorwbarros@gmail.com>
2023-09-21 01:59:20 -0300

    feat: :sparkles: create container micro component

---
+++ src/components/general/microComponents/Container.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div
+        class="rounded shadow"
+        :class="customClass"
+    >
+        <slot> </slot>
+    </div>
+</template>
+<script lang="ts">
+    export default {
+        props: {
+            customClass: { type: String, default: "bg-white" },
+        },
+    }
+</script>
+<style lang=""></style>
