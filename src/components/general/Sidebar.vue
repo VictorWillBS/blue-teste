d465cc53abfe09c83fbb7e5f9272a917f383f136
Victor Barros <victorwbarros@gmail.com>
2023-09-21 01:58:06 -0300

    refactor: :iphone: adjust sidebar responsivity

--- src/components/general/Sidebar.vue
+++ src/components/general/Sidebar.vue
@@ -1,43 +1,85 @@
-<template >
-  <aside class="bg-white h-full p-3 w-40 flex-none max-md:hidden shadow">
-    <article class="flex justify-center items-center w-full mb-20">
-      <img src="../../assets/blue-logo.svg"
-           class="w-25 h-8">
-    </article>
-    <section class="flex justify-center items-center">
-      <ul class="">
-        <a class="flex justify-start items-center w-full mb-4 ">
-          <ViewDashboard size="30"
-                         class="me-3" />
-          <span class="text-sm font-bold">Dashboard</span>
-        </a>
-        <a class="flex justify-start items-center w-full text-gray-500 mb-4">
-          <Table size="30"
-                 class="me-3" />
-          <span class="text-sm font-bold">Vendas</span>
-        </a>
-        <a class="flex justify-start items-center w-full text-gray-500 mb-4">
+<script setup lang="ts">
+    import { onMounted } from "vue"
+    import ViewDashboard from "vue-material-design-icons/ViewDashboard.vue"
+    import MoneyBagIcon from "../../../public/icons/MoneyBag.vue"
 
-          <MoneyBagIcon size="30"
-                        class="me-3" />
-          <span class="text-sm font-bold">Planos</span>
-        </a>
-      </ul>
-    </section>
-  </aside>
+    const emit = defineEmits(["updateSidebarVisibility"])
+    onMounted(function () {
+        window.addEventListener("resize", function () {
+            emit("updateSidebarVisibility", screen.width >= 1024)
+        })
+    })
+</script>
+<template>
+    <aside
+        class="absolute left-0 top-0 z-10 flex h-full min-h-screen w-full lg:w-fit"
+        :class="mobileSidebarOpen ? '' : 'hidden'"
+    >
+        <section class="h-full w-40 flex-none bg-white p-3 shadow lg:block">
+            <article
+                class="mb-20 flex w-full items-center justify-center"
+                @click="hideMobile()"
+            >
+                <img
+                    src="../../assets/blue-logo.svg"
+                    class="w-25 h-8"
+                />
+            </article>
+            <section class="flex items-center justify-center">
+                <ul class="">
+                    <a class="mb-4 flex w-full items-center justify-start">
+                        <ViewDashboard
+                            :size="30"
+                            class="me-3"
+                        />
+                        <span class="text-sm font-bold">Dashboard</span>
+                    </a>
+                    <a
+                        class="mb-4 flex w-full items-center justify-start text-gray-500"
+                    >
+                        <TableIcon
+                            :size="30"
+                            class="me-3"
+                        />
+                        <span class="text-sm font-bold">Vendas</span>
+                    </a>
+                    <a
+                        class="mb-4 flex w-full items-center justify-start text-gray-500"
+                    >
+                        <MoneyBagIcon
+                            :size="30"
+                            class="me-3"
+                        />
+                        <span class="text-sm font-bold">Planos</span>
+                    </a>
+                </ul>
+            </section>
+        </section>
+        <div
+            class="w-full bg-black bg-opacity-5 lg:hidden"
+            @click="hideMobile()"
+        ></div>
+    </aside>
 </template>
 <script lang="ts">
-import ViewDashboard from 'vue-material-design-icons/ViewDashboard.vue';
-import Table from 'vue-material-design-icons/Table.vue';
-import MoneyBagIcon from "../../assets/icons/MoneyBag.vue"
-export default {
-  components: {
-    ViewDashboard, Table, MoneyBagIcon
-  }
-};
+    import Table from "vue-material-design-icons/Table.vue"
+
+    export default {
+        props: ["mobileSidebarOpen"],
+        components: { TableIcon: Table },
+        methods: {
+            hideMobile() {
+                console.log(this.mobileSidebarOpen)
+                this.$emit("updateSidebarVisibility", false)
+                console.log(this.mobileSidebarOpen)
+
+                return
+            },
+        },
+    }
 </script>
 <style lang="css">
-.icon {
-  fill: #6b7280;
-}
+    .icon {
+        fill: #6b7280;
+    }
 </style>
