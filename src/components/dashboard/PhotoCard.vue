acd120ec5b1fcb848e99f9540fded9cae710139e
Victor Barros <victorwbarros@gmail.com>
2023-09-21 01:56:50 -0300

    feat: :sparkles:  create news aside photo_cards component

---
+++ src/components/dashboard/PhotoCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    import Container from "../general/microComponents/Container.vue"
+</script>
+
+<template lang="">
+    <Container custom-class="self-start lg:h-fit max-w-xs w-full rounded-lg">
+        <img
+            class="h-28 w-full rounded-t-lg object-cover"
+            :src="imageUrl"
+        />
+        <div class="p-2">
+            <div>
+                <p class="mb-1 text-xs font-bold text-violet-700">
+                    {{ title }}
+                </p>
+                <p class="text-wrap mb-3 text-xs text-gray-800">
+                    {{ description }}
+                </p>
+            </div>
+            <div class="text-end">
+                <a
+                    :href="link"
+                    class="text-xs font-bold text-violet-700"
+                    >Saiba mais</a
+                >
+            </div>
+        </div>
+    </Container>
+</template>
+<script lang="ts">
+    export default {
+        props: {
+            title: { type: String, default: "" },
+            description: { type: String, default: "" },
+            imageUrl: { type: String, default: "" },
+            link: { type: String, default: "" },
+        },
+    }
+</script>
+<style lang=""></style>
