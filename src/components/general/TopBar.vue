1412c6a7ee37edcaef9f0bd4f1a093c870630e26
Victor Barros <victorwbarros@gmail.com>
2023-09-21 01:58:59 -0300

    feat: :sparkles: add sidebar mobile button in top bar container

---
+++ src/components/general/TopBar.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    <section
+        class="mb-3 flex h-20 w-full max-w-7xl items-center justify-between"
+    >
+        <section class="flex gap-3">
+            <MenuIcon
+                class="cursor-pointer lg:hidden"
+                @click="showMobileSidebar()"
+            />
+            <article class="flex items-center text-sm font-bold text-white">
+                {{ date }}
+            </article>
+        </section>
+
+        <article class="">
+            <img
+                src="/images/happy_woman.jpg"
+                alt=""
+                class="h-11 min-h-11 w-11 min-w-11 rounded-full border-2 border-solid border-white object-cover"
+            />
+        </article>
+    </section>
+</template>
+<script lang="ts">
+    import Menu from "vue-material-design-icons/Menu.vue"
+    import getTodayDateHumanized from "../../utils/getTodayDateHumanized"
+
+    export default {
+        components: { MenuIcon: Menu },
+        data() {
+            return { date: getTodayDateHumanized() }
+        },
+        props: { mobileSidebarOpen: Boolean },
+        methods: {
+            showMobileSidebar() {
+                console.log(this.mobileSidebarOpen)
+                this.$emit("updateSidebarVisibility", true)
+                console.log(this.mobileSidebarOpen)
+
+                return
+            },
+        },
+    }
+</script>
+<style lang=""></style>
